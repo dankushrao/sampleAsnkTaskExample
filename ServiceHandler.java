03c9baf4b7e8635272ba4751364722e6c2452ffd
Dhananjay Ankushrao <dhananjay.ankushrao@gmail.com>
2016-06-29 14:33:38 +0530

    xxxxxxx

--- ServiceHandler.java
+++ ServiceHandler.java
@@ -17,7 +17,6 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
